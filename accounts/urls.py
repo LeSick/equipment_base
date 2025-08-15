c7314f8315978580bd23c74947002fb93812edde
Alexey Danilin <aleksey.danilin@gmail.com>
2025-08-15 11:14:29 +0300

    Changes to be committed: 	new file:   accounts/migrations/0001_initial.py 	modified:   accounts/urls.py 	modified:   templates/accounts/login.html 	modified:   templates/accounts/request_access.html 	modified:   templates/header.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 from .views import registration_request_view
+from django.contrib.auth import views as auth_views
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('request-access/', registration_request_view, name='request_access'),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='accounts/login.html',
+        redirect_authenticated_user=False
+    ), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(
+        next_page='accounts:login',
+        http_method_names=['get', 'post']
+    ), name='logout'),
 ]
